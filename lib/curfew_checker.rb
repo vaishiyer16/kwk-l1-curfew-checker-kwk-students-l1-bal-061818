41848843b3b76c95f929b73bc144480ac53a57e8
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-20 13:00:55 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -3,21 +3,21 @@ def simple_curfew_checker(time)
     puts "You're not late!"# code goes here
   else
     puts "Keep having fun!"
-  end   
+  end
 end
 
-def curfew_checker(time)
+#def curfew_checker(time)
   # code goes here
-end
+#end
 
-def complex_curfew_checker(time)
+#def complex_curfew_checker(time)
   # code goes here
-end
+#end
 
-def deluxe_curfew_checker(time)
+#def deluxe_curfew_checker(time)
   # code goes here
-end
+#end
 
-def platinum_curfew_checker(current_time, curfew_time)
+#def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
-end
+#end
